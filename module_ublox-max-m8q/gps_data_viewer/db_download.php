0f048c4f888c6cc56ad2772b1255683506917a4c
3cat2 <cubecat.vcn@gmail.com>
2016-09-16 12:45:06 +0200

    New files in gps_data_viewer

---
+++ module_ublox-max-m8q/gps_data_viewer/db_download.php
@@ -0,0 +1,22 @@
+<?php
+	
+    // Creating database variable
+    require("mysql-db-wrapper/config.inc.php");
+    require("mysql-db-wrapper/Database.class.php");
+    $db = new Database(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
+
+    // Connecting db
+    $db->connect();
+
+    // Get last GPS data
+    $gps = array();
+    $sql = "SELECT * FROM ".TABLE_GPS." WHERE `time` > {$_GET['date']} ORDER BY `time` ASC";
+    $query = $db->query($sql);
+    while($data = $db->fetch_array($query)) {
+    	$gps[] = [$data['lat'], $data['lng']];
+    }
+
+    print_r($gps);
+	// Disconnecting db
+    $db->close();
+?>
