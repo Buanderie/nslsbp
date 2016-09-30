804809a8286adde663ef8e24a48b1be4a4423071
Carles Araguz <carles.araguz@gmail.com>
2016-09-30 17:51:10 +0200

    JF screwed up

--- module_maps/mysql-db-wrapper/config.inc.php
+++ module_maps/mysql-db-wrapper/config.inc.php
@@ -3,12 +3,12 @@
 define('DB_SERVER', "localhost");
 
 // Database login name
-define('DB_USER', "barbie");
+define('DB_USER', "carles");
 // Database login password
-define('DB_PASS', "bbs");
+define('DB_PASS', "96452000");
 
 // Database name
-define('DB_DATABASE', "bbs");
+define('DB_DATABASE', "barbie_space_explorer");
 
 // Table names definition
 define('TABLE_GPS', "gps");
