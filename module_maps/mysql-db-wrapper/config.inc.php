e130d3d9ea476f24d14f80d9bf6b789987418866
chbs8 <christian.bs.8@gmail.com>
2017-02-24 11:26:17 +0100

    Teddy DB file added and MySQL wrapper folder

---
+++ module_maps/mysql-db-wrapper/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+//database server
+define('DB_SERVER', "localhost");
+
+//database login name
+define('DB_USER', "teddy");
+//database login password
+define('DB_PASS', "balloon");
+
+//database name
+define('DB_DATABASE', "teddy");
+
+//smart to define your table names also
+define('TABLE_HOUSEKEEPING', "");
+define('TABLE_GPS', "");
+?>
