616a6fb9af832956f12e19c0f6f9f832a3a16716
chbs8 <christian.bs.8@gmail.com>
2017-02-24 12:37:30 +0100

    PHP spaces reordered

--- module_maps/db_download.php
+++ module_maps/db_download.php
@@ -15,15 +15,15 @@ $gps = [];
 
 if($result->num_rows > 0) {
   while($data = $result->fetch_assoc()) {
-      $gps[] = [ $data['row_id'], $data['time_local'],
-                 $data['time_gps'], $data['lat'], $data['lng'], $data['gspeed'],
-                 $data['sea_alt'], $data['geo_alt'],
-                 $data['acc_x'], $data['acc_y'], $data['acc_z'],
-                 $data['gyro_x'], $data['gyro_y'], $data['gyro_z'],
-                 $data['mag_x'], $data['mag_y'], $data['mag_z'],
-                 $data['cpu_temp'], $data['gpu_temp'],
-                 $data['in_temp'], $data['in_pressure'],
-                 $data['out_temp'], $data['out_pressure'] ];
+      $gps[] = [$data['row_id'], $data['time_local'],
+                $data['time_gps'], $data['lat'], $data['lng'], $data['gspeed'],
+                $data['sea_alt'], $data['geo_alt'],
+                $data['acc_x'], $data['acc_y'], $data['acc_z'],
+                $data['gyro_x'], $data['gyro_y'], $data['gyro_z'],
+                $data['mag_x'], $data['mag_y'], $data['mag_z'],
+                $data['cpu_temp'], $data['gpu_temp'],
+                $data['in_temp'], $data['in_pressure'],
+                $data['out_temp'], $data['out_pressure'] ];
   }
 }
 
