7e258673dd074ccfcaa443448e15fea756f670c9
juan0fran <juan0fran@github.com>
2017-02-28 19:23:07 +0100

    vitow_stream_test up

--- module_vitow/vitow_stream_test.sh
+++ module_vitow/vitow_stream_test.sh
@@ -1,5 +1,9 @@
 tail -f vitow_output | pv -L 56250 | \
     ffmpeg  -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 \
-            -i /dev/zero -f h264 -i - -i watermark_no_barbie.png -filter_complex 'overlay=0:0' \
+            -i /dev/zero -f h264 \
+            -i - \
+            -i countdown.flv \
+            -i watermark_no_barbie.png \
+            -filter_complex "[1:v][2:v] overlay=[tmp]; [tmp][3:v] overlay=0:0" \
             -vcodec h264 -profile:v high -level 4.0 -acodec aac -ab 128k -g 50 -strict experimental \
             -f flv rtmp://a.rtmp.youtube.com/live2/rz6y-ezye-u0jy-93u6
