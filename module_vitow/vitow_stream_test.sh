5644079119806412902529523b501a92862c5bfe
juan0fran <juan0fran@github.com>
2017-03-01 19:29:17 +0100

    stream test with correct options

--- module_vitow/vitow_stream_test.sh
+++ module_vitow/vitow_stream_test.sh
@@ -1,8 +1,10 @@
 tail -f vitow_output | pv -L 56250 | \
     ffmpeg  -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
             -i - \
-            -ss 00:10:00 -i countdown.flv \
+            -ss 00:09:00 -i countdown.flv \
             -i watermark_no_barbie.png \
-            -filter_complex "[1:v][2:v] overlay=[tmp]; [tmp][3:v] overlay=0:0" \
-            -vcodec h264 -profile:v high -level 4.0 -acodec aac -ab 128k -g 50 -strict experimental \
-            -f flv rtmp://a.rtmp.youtube.com/live2/rz6y-ezye-u0jy-93u6
+            -filter_complex "[1:v][2:v] overlay=[tmp]; [tmp][3:v] overlay=[out]; [out]split=2[out1][out2]" \
+            -map "[out1]" -map 0:a -vcodec h264 -maxrate 1024k -bufsize 2048k -profile:v high -level 4.0 -acodec aac -ab 128k -g 50 -strict experimental \
+            -f flv rtmp://a.rtmp.youtube.com/live2/rz6y-ezye-u0jy-93u6 \
+            -map "[out2]" -map 0:a -vcodec h264 -preset ultrafast -maxrate 1024k -bufsize 2048k -profile:v high -level 4.0 -acodec aac -ab 128k -g 50 -strict experimental \
+            -f mpegts "udp://localhost:5000"
